39ac42fcecf749564488828ba428a974359e0d7a
Sebastian Patitucci <patitucciivo@gmail.com>
2024-11-22 10:09:53 -0300

    Servicios, letras y foto

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -71,20 +71,15 @@ export class ContactoComponent {
 }
 
 
-text = ''; // Texto que se mostrará con el efecto de máquina de escribir
-  originalText = 'Nuestro ';
-  text2 = ''; // Texto que se mostrará con el efecto de máquina de escribir
-  originalText2 = 'Contacto';
+  text = ''; // Texto que se mostrará con el efecto de máquina de escribir
+  originalText = 'Contacto';
   index = 0;
-  index2 = -7;
-  speed = 80; // Velocidad en milisegundos entre cada letra
+  speed = 150; // Velocidad en milisegundos entre cada letra
 
   typeWriter(): void {
-    if (this.index2 < this.originalText.length) {
+    if (this.index < this.originalText.length) {
       this.text += this.originalText.charAt(this.index);
-      this.text2 += this.originalText2.charAt(this.index2);
       this.index++;
-      this.index2++;
       setTimeout(() => {
         this.typeWriter();
       }, this.speed);
--- src/app/components/servicios/servicios.component.ts
+++ src/app/components/servicios/servicios.component.ts
@@ -8,23 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class ServiciosComponent implements OnInit{
 
   text = ''; // Texto que se mostrará con el efecto de máquina de escribir
-  originalText = 'Nuestros ';
-  text2 = ''; // Texto que se mostrará con el efecto de máquina de escribir
-  originalText2 = 'Servicios';
+  originalText = 'Servicios';
+  // text2 = ''; // Texto que se mostrará con el efecto de máquina de escribir
+  // originalText2 = 'Servicios';
   index = 0;
-  index2 = -7;
-  speed = 80; // Velocidad en milisegundos entre cada letra
+  // index2 = -7;
+  speed = 150; // Velocidad en milisegundos entre cada letra
 
   ngOnInit(): void {
     this.typeWriter();
   }
 
   typeWriter(): void {
-    if (this.index2 < this.originalText.length) {
+    if (this.index < this.originalText.length) {
       this.text += this.originalText.charAt(this.index);
-      this.text2 += this.originalText2.charAt(this.index2);
+      // this.text2 += this.originalText2.charAt(this.index2);
       this.index++;
-      this.index2++;
+      // this.index2++;
       setTimeout(() => {
         this.typeWriter();
       }, this.speed);
